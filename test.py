49e774a6deffc150553031e33411d6479b8fe32f
Stangoff <32449361+Stangoff@users.noreply.github.com>
2019-02-06 18:39:04 +0100

    no class implementation

--- test.py
+++ test.py
@@ -1,54 +1,36 @@
 
-import tkinter as tk
+from tkinter import *
 
 result = ""
+normal_form_1 = ""
+normal_form_2 = ""
 
 
-def validate():
-    # Try and except statement is used
-    # for handling the errors like zero
-    # division error etc.
-
-        global result
-
-        if todo.logic1.get() is None or todo.logic2.get() is None:
-            result = "ko"
-        else:
-            result = "ok"
-            logic3.set(str(todo.logic1.get()) + str(todo.logic2.get()))
-        logic3.set(result)
-
-        #equation.set(total)
-
-        # initialze the result variable
-        # by empty string
-
-class Todo(tk.Tk):
-
-    def __init__(self):
-
-        super().__init__()
-
-        self.title("Logic Comparator Pro")
-        self.geometry("800x400")
-
-        self.logic1 = tk.StringVar()
-        self.logic2 = tk.StringVar()
-
-        self.name_label1 = tk.Label(self, text="Logic Proposition 1", pady=10, bg="light grey", relief='raised').pack(fill=tk.BOTH)
-        self.logic_entry1 = tk.Entry(self, bg="white", fg="black", textvariable=self.logic1).pack(fill=tk.BOTH)
-        self.name_label2 = tk.Label(self, text="Logic Proposition 2", pady=10, bg="light grey", relief='raised').pack(fill=tk.BOTH)
-        self.logic_entry2 = tk.Entry(self, bg="white", fg="black", textvariable=self.logic2).pack(fill=tk.BOTH)
-
-        self.logic1.set('enter your first logic expression')
-        self.logic2.set('enter your second logic expression')
 
 
 if __name__ == "__main__":
 
-    todo = Todo()
-    todo.configure(background="light blue")
-    logic3 = tk.StringVar()
-    bouton_validate = tk.Button(todo, text=' validate ', fg='white', bg='blue', bd=4, command=validate).pack(fill=tk.BOTH, padx=350, pady=5)
-    logic_entry3 = tk.Entry(bg="white", fg="black", textvariable=logic3).pack(fill=tk.BOTH, padx=350)
-    todo.mainloop()
+    gui = Tk()
+    gui.configure(background="light blue")
+    gui.title("Logic Comparator Pro")
+    gui.geometry("800x400")
+
+    logic1 = StringVar()
+    logic2 = StringVar()
+    logicResult = StringVar()
+
+    name_label1 = Label(gui, text="Logic Proposition 1", pady=10, bg="light grey", relief='raised').pack(fill=BOTH)
+    logic_entry1 = Entry(gui, bg="white", fg="black", textvariable=logic1).pack(fill=BOTH)
+    name_label2 = Label(gui, text="Logic Proposition 2", pady=10, bg="light grey", relief='raised').pack(fill=BOTH)
+    logic_entry2 = Entry(gui, bg="white", fg="black", textvariable=logic2).pack(fill=BOTH)
+    bouton_check = Button(gui, text=' Check ', fg='white', bg='blue', bd=4, command=check()).pack(fill=BOTH, padx=350, pady=5)
+    bouton_compare = Button(gui, text=' Compare ', fg='white', bg='green', bd=4, command=compare()).pack(fill=BOTH, padx=350, pady=5)
+
+    logic_entry3 = Entry(gui, bg="white", fg="black", textvariable=logicResult).pack(fill=BOTH, padx=350)
+
+    logic1.set('enter your first logic expression')
+    logic2.set('enter your second logic expression')
+
+    gui.mainloop()
+
+
