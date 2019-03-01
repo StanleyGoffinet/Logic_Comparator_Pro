56beb8e2c8bdaac420dcfe88f14bec233b49421e
Stangoff <32449361+Stangoff@users.noreply.github.com>
2019-03-01 20:15:34 +0100

    User Guide

--- LogicPro_tkinter.py
+++ LogicPro_tkinter.py
@@ -143,7 +143,24 @@ def reset():
     check_count = 0
     nb_variable = 0
 
-    #print(truth_table_list)
+
+def help_window():
+    """
+    function use for help the user to use Logic Comparator Pro
+    """
+    top = Toplevel(gui)
+    lab = Label(top, text="User Guide :")
+    lab.pack()
+    lab2 = Label(top, text="The conjunction symbol is : '&' (key 1 on the keyboard) ")
+    lab2.pack()
+    lab3 = Label(top, text="The disjunction symbol is : '|' (Alt Gr + key 1 on the keyboard)")
+    lab3.pack()
+    lab4 = Label(top, text="The negatif symbol is : '-' (the dash)")
+    lab4.pack()
+    lab4 = Label(top, text="You can use the logic form too. Example : (b ∧ ¬ a) ∨ (c ∧ ¬a) ")
+    lab4.pack()
+    lab4 = Label(top, text="The expressions need to be checked before compared ")
+    lab4.pack()
 
 
 def compare():
@@ -164,11 +181,6 @@ def compare():
 
         normal_form_1_format = format_logic(normal_form_1)
         normal_form_2_format = format_logic(normal_form_2)
-
-        #print(normal_form_1, id(normal_form_1))
-        #print(normal_form_1_format, id(normal_form_1_format))
-        #print(normal_form_2, id(normal_form_2))
-        #print(normal_form_2_format, id(normal_form_2_format))
 
         for e in range(0, len(logic_table_instance)):
             normal_form_1_format_copy = normal_form_1_format
@@ -226,6 +238,7 @@ if __name__ == "__main__":
     logic_entry1 = Entry(gui, bg="white", fg="black", textvariable=logic1).pack(fill=BOTH)
     name_label2 = Label(gui, text="Logic Proposition 2", pady=10, bg="light grey", relief='raised').pack(fill=BOTH)
     logic_entry2 = Entry(gui, bg="white", fg="black", textvariable=logic2).pack(fill=BOTH)
+
     bouton_check = Button(gui, text=' Check ', fg='white', bg='blue', bd=4, command=lambda: check()) \
         .pack(fill=BOTH, padx=350, pady=5)
     bouton_compare = Button(gui, text=' Compare ', fg='white', bg='green', bd=4, command=lambda: compare()) \
@@ -235,6 +248,8 @@ if __name__ == "__main__":
 
     bouton_reset = Button(gui, text=' Reset ', fg='white', bg='red', bd=4, command=lambda: reset()) \
         .pack(fill=BOTH, padx=350, pady=5)
+    bouton_question = Button(gui, text=' ? ', fg='black', bg='yellow', bd=4, command=lambda: help_window()) \
+        .pack(fill=BOTH, padx=380, pady=5)
 
     scrollbar = Scrollbar(gui)
     scrollbar.pack(fill=BOTH)
@@ -246,5 +261,4 @@ if __name__ == "__main__":
     #logic2.set('(b | c) & -a')
     #logic1.set('(b ∧ ¬ a) ∨ (c ∧ ¬a)')
     #logic2.set('(b ∨ c) ∧ ¬a')
-    #'∨', '∧' ¬
     gui.mainloop()
